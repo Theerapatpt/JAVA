9f6aeaa96ee8b6a1eb0bad7022ba4e8b32f9bf05
TheerapatNG <theerapat.dev@gmail.com>
2019-04-29 14:06:12 +0700

    initial commit code

---
+++ SportCar.java
@@ -0,0 +1,11 @@
+public class SportCar {
+    int modelYear;
+    String modelName;
+    String modelColor;
+
+    public SportCar(int year, String name, String color) {
+        modelName = name;
+        modelYear = year;
+        modelColor = color;
+    }
+}
---
+++ handleTest.java
@@ -0,0 +1,6 @@
+public class handleTest {
+    public static void main(String[] args) {
+        SportCar mysportcar = new SportCar(1923, "Mustang", "Brone");
+        System.out.println(mysportcar.modelYear + "-" + mysportcar.modelName + "-" + mysportcar.modelColor);
+    }
+}
