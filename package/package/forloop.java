3cf19ed59e22f68d8079921f10626bdb499c1958
TheerapatNG <theerapat.dev@gmail.com>
2019-04-29 14:56:43 +0700

    Add file all

---
+++ package/package/forloop.java
@@ -0,0 +1,7 @@
+public class forloop {
+    public static void main(String[] args) {
+        for (int xenial = 0; xenial < 20; xenial++) {
+            System.out.println("Total Xenial is : " + xenial);
+        }
+    }
+}
